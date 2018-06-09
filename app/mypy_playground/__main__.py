c374324da4fd04cd03f0b908ff6aee2a737dc2f0
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-06-09 15:36:52 +0900

    Improve configuration loader

--- app/mypy_playground/__main__.py
+++ app/mypy_playground/__main__.py
@@ -7,17 +7,19 @@ from tornado.options import options, parse_command_line
 from .app import make_app
 
 
+def parse_environment_variables() -> None:
+    for option_name in options._options:
+        env_name = option_name.upper().replace("-", "_")
+        env_value = environ.get(env_name)
+        if env_value is None:
+            continue
+        option = options._options[option_name]
+        option.parse(env_value)
+
+
 def load_config() -> None:
     parse_command_line()
-
-    options.ga_tracking_id = environ.get("MYPY_PLAY_GA_TRACKING_ID")
-    options.github_token = environ.get("MYPY_PLAY_GITHUB_TOKEN")
-    port = environ.get("MYPY_PLAY_PORT")
-    if port is not None:
-        options.port = int(port)
-    debug = environ.get("MYPY_PLAY_DEBUG", "")
-    if debug != "0" and debug.lower() != "false":
-        options.debug = True
+    parse_environment_variables()
 
 
 load_config()
