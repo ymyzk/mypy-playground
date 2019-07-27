5696dc3c0886f043f16be28a2429fceead1b30a6
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-07-27 23:04:34 +0900

    Create neutrino project

---
+++ app/frontend/.neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'mypy Playground'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};
---
+++ app/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  state = {
+    name: 'frontend',
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to {this.state.name}</h1>
+      </div>
+    );
+  }
+}
