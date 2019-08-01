7c1cc90805b94ac3f8490ee4bca30e40f559f299
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-08-02 01:10:21 +0900

    Improve use of axios API in gist.js

--- app/frontend/src/gist.js
+++ app/frontend/src/gist.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 export async function shareGist(source) {
-  const response = await axios.post('/gist', { source });
-  if (response.status !== 201) {
-    throw new Error(`Unexpected status code: ${response.status}`);
-  }
+  const response = await axios.post('/gist', { source }, {
+    validateStatus(status) {
+      return status === 201;
+    },
+  });
   return {
     gistUrl: response.data.url,
     playgroundUrl: `https://play-mypy.ymyzk.com/?gist=${response.data.id}`,
@@ -12,10 +13,11 @@ export async function shareGist(source) {
 }
 
 export async function fetchGist(gistId) {
-  const response = await axios.get(`https://api.github.com/gists/${gistId}`);
-  if (response.status !== 200) {
-    throw new Error(`Cannot fetch the gist with ID: ${gistId}`);
-  }
+  const response = await axios.get(`https://api.github.com/gists/${gistId}`, {
+    validateStatus(status) {
+      return status === 200;
+    },
+  });
   if (!('main.py' in response.data.files)) {
     throw new Error('"main.py" not found');
   }
