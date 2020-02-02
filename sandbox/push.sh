0d1c99d022dcd059ed50d693892aed85ba10cdc3
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2020-02-02 23:19:57 +0900

    Build all sandbox images on GitHub Actions

---
+++ sandbox/push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+
+if [ -n "$1" ]; then
+  image_name="$1"
+else
+  image_name="mypy-playground-sandbox"
+fi
+
+set -u
+find . -maxdepth 1 -mindepth 1 -type d,l -exec bash -c '
+  dir="$1"
+  tag=$(basename "$dir")
+  docker push "$2:$tag"
+' _ {} "$image_name" \;
