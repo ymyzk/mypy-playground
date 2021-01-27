f001755c00eef182de6baf3105b2c56181464400
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2021-01-27 23:24:27 +0900

    Refactor and add tests for sandbox

--- app/mypy_playground/sandbox/__init__.py
+++ app/mypy_playground/sandbox/__init__.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 semaphore: Optional[asyncio.Semaphore] = None
 
 
-def get_semaphore() -> asyncio.Semaphore:
+def _get_semaphore() -> asyncio.Semaphore:
     # Lazy initialization of the semaphore to use options correctly
     global semaphore
     if not semaphore:
@@ -23,11 +23,17 @@ def get_semaphore() -> asyncio.Semaphore:
     return semaphore
 
 
-async def run_typecheck_in_sandbox(sandbox: AbstractSandbox,
-                                   source: str,
-                                   **kwargs: Any
-                                   ) -> Optional[Result]:
+async def run_typecheck_in_sandbox(
+        sandbox: AbstractSandbox,
+        source: str,
+        semaphore: Optional[asyncio.Semaphore] = None,
+        **kwargs: Any
+) -> Optional[Result]:
+    if semaphore is None:
+        logger.debug("using the default semaphore")
+        semaphore = _get_semaphore()
+
     logger.debug("acquiring semaphore")
-    async with get_semaphore():
+    async with semaphore:
         logger.debug("acquired semaphore")
         return await sandbox.run_typecheck(source, **kwargs)
--- app/mypy_playground/sandbox/docker.py
+++ app/mypy_playground/sandbox/docker.py
@@ -19,19 +19,9 @@ class DockerSandbox(AbstractSandbox):
     source_file_path: Path
 
     def __init__(self) -> None:
+        super().__init__()
         self.client = aiodocker.Docker()
         self.source_file_path = Path("/tmp/main.py")
-
-    def create_archive(self, source: str) -> BytesIO:
-        stream = BytesIO()
-        with tarfile.TarFile(fileobj=stream, mode="w") as tar:
-            data = source.encode("utf-8")
-            tarinfo = tarfile.TarInfo(name=self.source_file_path.name)
-            tarinfo.size = len(data)
-            tarinfo.mtime = int(time.time())
-            tar.addfile(tarinfo, BytesIO(data))
-        stream.seek(0)
-        return stream
 
     async def run_typecheck(self,
                             source: str,
@@ -40,7 +30,9 @@ class DockerSandbox(AbstractSandbox):
                             python_version: Optional[str] = None,
                             **kwargs: Any
                             ) -> Optional[Result]:
-        docker_image = self.get_docker_image(mypy_version)
+        start_time = time.time()
+
+        docker_image = self._get_docker_image(mypy_version)
         if docker_image is None:
             logger.error(f"cannot find a docker image for mypy version: {mypy_version}")
             return None
@@ -52,39 +44,60 @@ class DockerSandbox(AbstractSandbox):
             if key in ARGUMENT_FLAGS:
                 cmd.append(f"--{key}")
         cmd.append(self.source_file_path.name)
-        try:
-            start_time = time.time()
-            logger.info("creating container")
-            config = {
-                "Image": docker_image,
-                "Cmd": cmd,
-                "HostConfig": {
-                    "CapDrop": ["ALL"],
-                    "Memory": 128 * 1024 * 1024,
-                    "NetworkMode": "none",
-                    "PidsLimit": 32,
-                    "SecurityOpt": ["no-new-privileges"]
-                }
+
+        config = {
+            "Image": docker_image,
+            "Cmd": cmd,
+            "HostConfig": {
+                "CapDrop": ["ALL"],
+                "Memory": 128 * 1024 * 1024,
+                "NetworkMode": "none",
+                "PidsLimit": 32,
+                "SecurityOpt": ["no-new-privileges"]
             }
+        }
+
+        # Using Any to suppress type errors around aiodocker
+        c: Optional[Any] = None
+        try:
+            logger.info("creating container")
             c = await self.client.containers.create(config=config)  # type: ignore
             await c.put_archive(str(self.source_file_path.parent),
-                                self.create_archive(source))
+                                self._create_archive(source))
             await c.start()
             exit_code = (await c.wait())["StatusCode"]
             stdout = "".join(await c.log(stdout=True, stderr=False)).strip()
             stderr = "".join(await c.log(stdout=False, stderr=True)).strip()
             await c.delete()
-            duration = int(1000 * (time.time() - start_time))
-            logger.info("finished in %d ms", duration)
-            return Result(
-                exit_code=exit_code,
-                stdout=stdout,
-                stderr=stderr,
-                duration=duration)
-        except aiodocker.exceptions.DockerError as e:
-            logger.error(f"docker api error: {e}")
-        # TODO: better error handling
-        return None
+        except aiodocker.exceptions.DockerError:
+            logger.exception("docker api error")
+            if c is not None:
+                try:
+                    logger.info("cleaning up the container: %s", c)
+                    await c.delete()
+                except aiodocker.exceptions.DockerError:
+                    logger.exception("docker api error while cleaning up. ignoring.")
+            return None
 
-    def get_docker_image(self, mypy_version_id: str) -> Optional[str]:
+        duration = int(1000 * (time.time() - start_time))
+        logger.info("finished in %d ms", duration)
+        return Result(
+            exit_code=exit_code,
+            stdout=stdout,
+            stderr=stderr,
+            duration=duration,
+        )
+
+    def _create_archive(self, source: str) -> BytesIO:
+        stream = BytesIO()
+        with tarfile.TarFile(fileobj=stream, mode="w") as tar:
+            data = source.encode("utf-8")
+            tarinfo = tarfile.TarInfo(name=self.source_file_path.name)
+            tarinfo.size = len(data)
+            tarinfo.mtime = int(time.time())
+            tar.addfile(tarinfo, BytesIO(data))
+        stream.seek(0)
+        return stream
+
+    def _get_docker_image(self, mypy_version_id: str) -> Optional[str]:
         return parse_option_as_dict("docker_images").get(mypy_version_id)
