2a4f9fb74776a2cde4cd79a58d551d4c7b463b74
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2021-07-19 23:28:52 +0900

    Add Cloud Functions sandbox

---
+++ sandbox/cloud_functions/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -eu
+
+cd "$(dirname "$0")"
+
+: "${FUNCTION_NAME_BASE:=mypy-}"
+: "${MAX_INSTANCES:=3}"
+: "${MEMORY:=1024MB}"
+: "${REGION:=asia-northeast1}"
+: "${RUNTIME:=python39}"
+
+deploy() {
+  VERSION="$1"
+  # We cannot use "." in a function name.
+  FUNCTION_NAME="${FUNCTION_NAME_BASE}${VERSION//./-}"
+  echo "Deploying ${VERSION} as ${FUNCTION_NAME}..."
+  gcloud functions deploy "${FUNCTION_NAME}" \
+    --trigger-http \
+    --entry-point=run_typecheck \
+    --security-level=secure-always \
+    --quiet \
+    "--region=${REGION}" \
+    "--runtime=${RUNTIME}"\
+    "--memory=${MEMORY}" \
+    "--source=${VERSION}" \
+    "--service-account=${SERVICE_ACCOUNT}" \
+    "--max-instances=${MAX_INSTANCES}"
+  echo "Updating IAM policy for ${FUNCTION_NAME}..."
+  gcloud functions add-iam-policy-binding "${FUNCTION_NAME}" \
+    "--region=${REGION}" \
+    "--member=${INVOKER_MEMBER}" \
+    --role=roles/cloudfunctions.invoker
+}
+
+for ver in 0.*; do
+  deploy "$ver"
+done
+deploy latest
