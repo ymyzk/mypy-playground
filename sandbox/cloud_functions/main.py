1297b250c74452fbe4676e7815f89d47ec6f720d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2021-11-14 13:09:26 +0900

    Revert change in sandbox/cloud_functions/main.py

--- sandbox/cloud_functions/main.py
+++ sandbox/cloud_functions/main.py
@@ -1,7 +1,13 @@
+"""
+Wrapper to run mypy on Cloud Funcions.
+
+This module should be able to run on Python 3.7 and later.
+"""
+
 import subprocess
 import sys
 import tempfile
-from typing import NoReturn
+from typing import Dict, List, Union
 
 from flask import Request, Response, abort, jsonify, make_response
 
@@ -27,7 +33,7 @@ def run_typecheck(request: Request) -> Response:
     return jsonify(run_mypy(source, options))
 
 
-def run_mypy(source: str, options: list[str]) -> dict[str, int | str]:
+def run_mypy(source: str, options: List[str]) -> Dict[str, Union[int, str]]:
     with tempfile.NamedTemporaryFile(mode="w") as f:
         f.write(source)
         f.flush()
@@ -42,5 +48,5 @@ def run_mypy(source: str, options: list[str]) -> dict[str, int | str]:
         }
 
 
-def abort_api(status: int, message: str) -> NoReturn:
+def abort_api(status: int, message: str) -> None:
     abort(make_response(jsonify(message=message), status))
