8fed5beff7166e193b457cae67416edcdd9195f3
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2021-12-28 21:33:37 +0900

    Automate process to add a new mypy version

---
+++ sandbox/add_version.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+readonly sandbox_dir="$(pwd)"
+
+if [ -z "$1" ]; then
+  echo "usage: $0 <version>" 1>&2
+  exit 1
+fi
+
+set -u
+readonly version="$1"
+readonly cloud_functions_dir="${sandbox_dir}/cloud_functions/${version}"
+readonly docker_dir="${sandbox_dir}/docker/${version}"
+
+if [ -d "$cloud_functions_dir" ]; then
+  echo "${cloud_functions_dir} already exists" 1>&2
+  exit 1
+fi
+if [ -d "$docker_dir" ]; then
+  echo "${docker_dir} already exists" 1>&2
+  exit 1
+fi
+
+set -x
+
+mkdir "$cloud_functions_dir"
+cd "$cloud_functions_dir"
+echo "mypy[python2]==${version}
+typing-extensions" > "requirements.in"
+pip-compile
+ln -s ../main.py ./
+cd "$sandbox_dir"
+
+mkdir "$docker_dir"
+cd "$docker_dir"
+cp "${cloud_functions_dir}/requirements.in" ./
+cp "${cloud_functions_dir}/requirements.txt" ./
+echo 'FROM python:3.10-slim
+
+WORKDIR /tmp
+COPY ./requirements.txt /tmp/
+
+RUN pip install -r requirements.txt \
+        && rm -rf /tmp/requirements.txt \
+        && rm -rf /root/.cache
+
+USER nobody
+CMD ["mypy"]' > "Dockerfile"
+cd "$sandbox_dir"
