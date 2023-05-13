24ffa07552109d6e00398316b6875e2a00afb09d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2023-05-13 11:16:44 +0900

    Add title and description

--- app/frontend/pages/index.tsx
+++ app/frontend/pages/index.tsx
@@ -1,4 +1,6 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+
+import Head from 'next/head';
 
 import App from '../components/App';
 import ContextLoader from '../components/ContextLoader';
@@ -7,6 +9,12 @@ const Home: NextPage = () => {
   const AppWithContext = ContextLoader(App);
   return (
     <div>
+      <Head>
+        <title>
+          mypy Playground
+        </title>
+        <meta name="description" content="The mypy Playground is a web service that receives a Python program with type hints, runs mypy inside a sandbox, then returns the output." />
+      </Head>
       <AppWithContext />
     </div>
   )
