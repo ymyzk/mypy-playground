8855de09b4fe5227a2683a2d2bc89129d4db10f8
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2024-08-02 12:14:31 +0900

    Add additional logging to deploy.sh

--- sandbox/cloud_functions/deploy.sh
+++ sandbox/cloud_functions/deploy.sh
@@ -29,7 +29,8 @@ deploy() {
     "--memory=${MEMORY}" \
     "--source=${VERSION}" \
     "--service-account=${SERVICE_ACCOUNT}" \
-    "--max-instances=${MAX_INSTANCES}"
+    "--max-instances=${MAX_INSTANCES}" \
+    "--verbosity=debug"
   echo "Updating IAM policy for ${FUNCTION_NAME}..."
   gcloud functions add-iam-policy-binding "${FUNCTION_NAME}" \
     "--region=${REGION}" \
