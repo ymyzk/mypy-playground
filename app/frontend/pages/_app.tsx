143bc8f380723a7effa2ef0cb2bf8c511b89a73d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2026-02-07 11:53:08 +0900

    Migrate frontend from Next.js to Vite

--- app/frontend/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
