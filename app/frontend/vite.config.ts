eb50f589514195f8f90dcc9acc9dd45ec52c0dda
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2026-02-07 12:09:26 +0900

    Update Docker-based local dev environment to use Vite

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,21 +1,25 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const apiServer = env.VITE_API_BASE_URL || "http://localhost:8080";
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: apiServer,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/test/setup.ts'],
-  },
-})
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: ["./src/test/setup.ts"],
+    },
+  };
+});
